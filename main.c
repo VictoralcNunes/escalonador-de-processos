df887a54b6c3e9665e83bcc66ca01d4121757c9c
Pedro Mello <pedromellogomes@gmail.com>
2018-05-29 23:43:45 -0300

    main while with getchar()

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "so.c"
 
 int main(){
-	
+
 	TF *fila_entrada = cria_fila();
 	TF *fila_pronto_real = cria_fila();
 	TF *fila_pronto_usuario = cria_fila();
@@ -14,16 +14,16 @@ int main(){
 	Recursos* recursos = cria_recursos();
 
 	char str[10]; // Variavel para armazenar o nome do arquivo a ser usado
-	printf("Digite o nome do arquivo: ");printf("\n");
+	printf("Digite o nome do arquivo: ");
 	scanf("%s", str);
-	
+
 	fila_entrada = armazena(fila_entrada, str);
-	
-	while(fila-entrada){
-		//	checar recursos-> momento == tempo de entrada do processo na frente da entrada 
+
+	while(getchar() != 0/*fila-entrada*/){
+		//	checar recursos-> momento == tempo de entrada do processo na frente da entrada
 		//com a função na_entrada
 
-		//	chamar escalonador de entrada dentro de um while que decrementa uma variável 
+		//	chamar escalonador de entrada dentro de um while que decrementa uma variável
 		//que guardou a  o valor de na_entrada, fazendo um pop da entrada
 
 		//	chamar o escalonador de curto prazo. Ele faz a decisão de qual politica vai usar
@@ -37,18 +37,18 @@ int main(){
 		//	a cada troca de estado, printar a mudança
 
 		//	a cada momento, printar o estado dos recursos
-		
-		recursos->momento++;	
+
+		recursos->momento++;
 	}
-	
-	
-	
+
+
+
 	imprime_fila(fila_entrada);
-	
-	
-	
+
+
+
 
 	libera_fila(fila_entrada);
 	return(0);
-	
+
 }
