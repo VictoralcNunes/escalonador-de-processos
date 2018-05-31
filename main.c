f6d33d029eb9a03e5ee046bbc9eec9e141aab8a3
BrunoXSantos <bruno_bittencourt_santos@hotmail.com>
2018-05-30 23:11:41 -0300

    coisas na main

--- main.c
+++ main.c
@@ -49,13 +49,13 @@ int main(){
 		//dependendo das filas de prontos
 
 		//	checar se um processo terminou e sem tem como passar um processor suspenso pra pronto
-
+/*
 		if (fila_pronto_real->processo->tempo_restante==0 || fila_pronto_usuario->processo->tempo_restante==0){
 			if (checa_disponibilidade(recursos, fila_pronto_suspenso->processo)){
 				escalonadorMedioVolta (fila_pronto_suspenso, fila_pronto_usuario);
 			}
 		}
-
+*/
 
 		//	checar se bloqueados e bloqueados suspensos podem voltar a estarem prontos ou prontos
 		//suspensos
